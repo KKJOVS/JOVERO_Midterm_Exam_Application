297bb5c1d19bd71da5fd30bb35f87a9a25c5a689
KKJOVS <natrovez0914@gmail.com>
2024-06-20 18:54:13 +0800

    Using Visual Studio Code: Editing user.php , controller.php, welcome.blade.php, and then web.php for  products.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,26 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        // Array of products containing the product name, its description and then price.
+        $products = [
+            ['name' => 'Orange Candy', 'description' => 'Delicious orange-flavored candy', 'price' => 'P 69'],
+            ['name' => 'Banana Candy', 'description' => 'Sweet banana-flavored candy', 'price' => 'P 55.10'],
+            ['name' => 'Apple Candy', 'description' => 'Tangy apple-flavored candy', 'price' => 'P 63.1'],
+            ['name' => 'Grapes Candy', 'description' => 'Juicy grapes-flavored candy', 'price' => 'P 71.22'],
+            ['name' => 'Strawberry Candy', 'description' => 'Fresh strawberry-flavored candy', 'price' => 'P 61.55'],
+            ['name' => 'Blueberry Candy', 'description' => 'Blueberry-flavored candy bursting with flavor', 'price' => 'P 75.55'],
+            ['name' => 'Melon Candy', 'description' => 'Cool melon-flavored candy', 'price' => 'P 66.65'],
+        ];
+
+        // products
+
+        return view('products.index', compact('products'));
+    }
 }
