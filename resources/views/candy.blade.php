12661156cd866bcf4f5d634d8f4b25fed00d5ab7
KKJOVS <natrovez0914@gmail.com>
2024-07-03 20:57:12 +0800

    replace some code

--- resources/views/candy.blade.php
+++ resources/views/candy.blade.php
@@ -3,19 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Candy Products</title>
+    <style>
+        .product {
+            border: 1px solid #ccc;
+            padding: 16px;
+            margin: 16px;
+            border-radius: 8px;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+        }
+        .product h2 {
+            margin-top: 0;
+        }
+        .product p {
+            margin: 8px 0;
+        }
+    </style>
 </head>
 <body>
     <h1>Candy Products</h1>
-    <ul>
-        @foreach ($products as $product)
-            <li>
-                <h2>{{ $product['name'] }}</h2>
-                <p>{{ $product['description'] }}</p>
-                <p>Price: {{ $product['price'] }}</p>
-            </li>
-        @endforeach
-    </ul>
+    @foreach ($products as $product)
+        <div class="product">
+            <h2>{{ $product['name'] }}</h2>
+            <p>{{ $product['description'] }}</p>
+            <p><strong>Price:</strong> {{ $product['price'] }}</p>
+        </div>
+    @endforeach
 </body>
 </html>
