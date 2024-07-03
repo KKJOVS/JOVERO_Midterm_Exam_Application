8f148bf6815d587ae30606c8673f2500886bff5c
KKJOVS <natrovez0914@gmail.com>
2024-07-03 19:51:58 +0800

    Updating products for midterm exam

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,25 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-        // Array of products containing the product name, its description and then price.
-        $products = [
-            ['name' => 'Orange Candy', 'description' => 'Delicious orange-flavored candy', 'price' => 'P 69'],
-            ['name' => 'Banana Candy', 'description' => 'Sweet banana-flavored candy', 'price' => 'P 55.10'],
-            ['name' => 'Apple Candy', 'description' => 'Tangy apple-flavored candy', 'price' => 'P 63.1'],
-            ['name' => 'Grapes Candy', 'description' => 'Juicy grapes-flavored candy', 'price' => 'P 71.22'],
-            ['name' => 'Strawberry Candy', 'description' => 'Fresh strawberry-flavored candy', 'price' => 'P 61.55'],
-            ['name' => 'Blueberry Candy', 'description' => 'Blueberry-flavored candy bursting with flavor', 'price' => 'P 75.55'],
-            ['name' => 'Melon Candy', 'description' => 'Cool melon-flavored candy', 'price' => 'P 66.65'],
-        ];
-
-        // products
-
-        return view('products.index', compact('products'));
+    public function show() {
+        return view('candy', [
+            'products' => [
+                [
+                    'name' => 'Orange Candy',
+                    'description' => 'A delicious and tangy orange flavored candy.',
+                    'price' => '₱50'
+                ],
+                [
+                    'name' => 'Apple Candy',
+                    'description' => 'A sweet and crispy apple flavored candy.',
+                    'price' => '₱45'
+                ],
+                [
+                    'name' => 'Banana Candy',
+                    'description' => 'A soft and chewy banana flavored candy.',
+                    'price' => '₱40'
+                ],
+                [
+                    'name' => 'Grapes Candy',
+                    'description' => 'A juicy and flavorful grapes flavored candy.',
+                    'price' => '₱55'
+                ]
+            ]
+        ]);
     }
 }
