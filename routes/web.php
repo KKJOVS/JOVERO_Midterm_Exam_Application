56b3714ad871f21738de6649bc6ebd3d4d3ff2a4
KKJOVS <natrovez0914@gmail.com>
2024-07-03 20:44:10 +0800

    re-update web.php routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\AnimeController;
 
 Route::get('/', [ProductController::class, 'show']);
